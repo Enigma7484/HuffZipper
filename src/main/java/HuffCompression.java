ab20eaa90533da1149646d60266e629eaf499b56
Enigma7484 <omarhaque7484@gmail.com>
2023-01-20 01:36:44 -0500

    Commit 4

--- src/main/java/HuffCompression.java
+++ src/main/java/HuffCompression.java
@@ -48,7 +48,86 @@ public class HuffCompression {
             while (nodes.len() > 1) {
                 ByteNode left = nodes.poll();
                 ByteNode right = nodes.poll();
+                ByteNode parent = new ByteNode(null, left.frequency + right.frequency);
+                parent.left = left;
+                parent.right = right;
+                nodes.add(parent);
+            }
+            return nodes.poll();
+        }
 
+        private static Map<Byte, String> getHuffCodes(ByteNode root) {
+            if (root == null) return null;
+            getHuffCodes(root.left, "0", sb);
+            getHuffCodes(root.right, "1", sb);
+            return huffmap;
+        }
+
+        private static void getHuffCodes(ByteNode node, String code, StringBuilder sb1) {
+            StringBuilder sb2 = new StringBuilder(sb1);
+            sb2.append(code);
+            if (node != null) {
+                if (node.data == null) {
+                    getHuffCodes(node.left, "0", sb2);
+                    getHuffCodes(node.right, "1", sb2);
+                } else
+                    huffmap.put(node.data, sb2.toString());
+            }
+        }
+
+        private static byte[] zipBytesWithCodes(byte[] bytes, Map<Byte, String> huffCodes) {
+            StringBuilder strBuilder = new StringBuilder();
+            for (byte b : bytes)
+                strBuilder.append(huffCodes.get(b));
+
+            int length=(strBuilder.length()+7)/8;
+            byte[] huffCodeBytes = new byte[length];
+            int index = 0;
+            for (int i = 0; i < strBuilder.length(); i += 8) {
+                String strByte;
+                if (i + 8 > strBuilder.length())
+                    strByte = strBuilder.substring(i);
+                else strByte = strBuilder.substring(i, i + 8);
+                huffCodeBytes[index] = (byte) Integer.parseInt(strByte, 2);
+                index++;
+            }
+            return huffCodeBytes;
+        }
+
+        public static void decompress(String src, String dst) {
+            try {
+                FileInputStream inStream = new FileInputStream(src);
+                ObjectInputStream objectInStream = new ObjectInputStream(inStream);
+                byte[] huffmanBytes = (byte[]) objectInStream.readObject();
+                Map<Byte, String> huffmanCodes = (Map<Byte, String>) objectInStream.readObject();
+                byte[] bytes = decomp(huffmanCodes, huffmanBytes);
+                OutputStream outStream = new FileOutputStream(dst);
+                outStream.write(bytes);
+                inStream.close();
+                objectInStream.close();
+                outStream.close();
+            }   catch (Exception e) { e.printStackTrace(); }
+        }
+
+        public static byte[] decomp(Map<Byte, String> huffmanCodes, byte[] huffmanBytes) {
+            StringBuilder sb1 = new StringBuilder();
+            for (int i = 0; i < huffmanBytes.length; i++) {
+                byte b = huffmanBytes[i];
+                boolean flag = (i == huffmanBytes.length - 1);
+                sb1.append(convertbyteInBit(!flag, b));
+            }
+            Map<String, Byte> map = new HashMap<>();
+            for (Map.Entry<Byte, String> entry : huffmanCodes.entrySet()) {
+                map.put(entry.getValue(), entry.getKey());
+            }
+            java.util.List<Byte> list = new java.util.ArrayList<>();
+            for (int i = 0; i < sb1.length();) {
+                int count = 1;
+                boolean flag = true;
+                Byte b = null;
+                while (flag) {
+                    String key = sb1.substring()
+                }
             }
         }
 }
--- src/main/java/MinPriorityQueue.java
+++ src/main/java/MinPriorityQueue.java
@@ -7,8 +7,69 @@ public class MinPriorityQueue<T extends Comparable<T>> {
 
     public int len() { return size; }
     public boolean isEmpty() { return  size == 0; }
-    public void add(T byteNode) {
+    public T poll() { return poll(0); }
+    public void add(T elem) {
+        list.add(elem);
+        bubbleUp(size);
+        size++;
     }
 
+    private T poll(int i) {
+        if(size == 1) {
+            size--;
+            T data = list.get(0);
+            list.clear();
+            return data;
+        }
+        size--;
+        T data_to_return = list.get(i);
+        swap(i, size);
+        list.remove(list.get(size));
+        bubbleDown(i);
+        return data_to_return;
+    }
 
+    private void bubbleDown(int k) {
+        while(true) {
+            int left = (k*2) + 1;
+            int right = (k*2) + 2;
+            int smallest = left;
+            if(right < size) {
+                if (isLess(right, left)) smallest = right;
+            }
+            if(smallest < size) {
+                if (isLess(k, smallest)) break;
+                swap(k, smallest);
+                k = smallest;
+            } else break;
+        }
+    }
+
+    private void bubbleUp(int k) {
+        int parent_index = (k-1) / 2;
+        while(k > 0 && isLess(k, parent_index)) {
+            swap(k, parent_index);
+            k = parent_index;
+            parent_index = (k-1) / 2;
+        }
+    }
+
+    private boolean isLess(int i, int j) {
+        T e0 = list.get(i);
+        T e1 = list.get(j);
+
+        return e0.compareTo(e1) <= 0;
+    }
+
+    private void swap(int i, int j) {
+        T e_i = list.get(i);
+        T e_j = list.get(j);
+
+        list.set(i, e_j);
+        list.set(j, e_i);
+    }
+
+    public String toString() {
+        return list.toString();
+    }
 }
